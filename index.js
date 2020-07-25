31be71562d3c78e39c9bb8380fed19721119ad94
Andrew Xue <andewzxue@gmail.com>
2020-07-25 14:43:16 -0400

    checklist and check file type

--- index.js
+++ index.js
@@ -19,19 +19,33 @@ app.get('/', (req, res) => {
 });
 
 //post request from submitting image
-app.post('/submit', function(req, res) {
+app.post('/submit', (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send('No files were uploaded.');
   }
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let sampleFile = req.files.file;
-  
+  let type = sampleFile.mimetype;
+  if (type.substring(0, type.indexOf("/")) != "image") {
+    return res.status(500).send("Not an image");
+  }
+
   // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(__dirname+'/present.jpg', function(err) {
-    if (err)
-      return res.status(500).send(err);
-  });
+  sampleFile.mv(__dirname+'/present.jpg');
+  var checklist = {
+    "children": req.body.children,
+    "teen": req.body.teen,
+    "adult": req.body.adult,
+    "foodRelated": req.body.foodRelated,
+    "giftBasket": req.body.giftBasket,
+    "bDayCards": req.body.bDayCards,
+    "booksMovies": req.body.booksMovies,
+    "colors": req.body.colors,
+    "sports": req.body.sports,
+    "music": req.body.music,
+    "art": req.body.art
+  }
 
   //performs label detection
   client
@@ -47,7 +61,7 @@ app.post('/submit', function(req, res) {
         "score": label['score']
       });
     });
-    console.log(labelList);
+
     var worthiness = 1234; //foo(checklist, labelList);
     res.render('results.ejs', {worthiness: worthiness});
   })
